1b6f494f99614788232f148ebd4df85df8643266
egekol <egekol@hotmail.com>
2019-12-20 23:54:19 +0300

    Unity project

---
+++ Unity Workshop/Assets/Sc_materialcolour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Sc_materialcolour : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+        }
+    }
+}
